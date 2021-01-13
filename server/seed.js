02279815a9fc22421a653b222e7a4f9adabe0534
joshuabradley012 <joshuabradley012@gmail.com>
2021-01-12 20:54:18 -0800

    Started seed script

---
+++ server/seed.js
@@ -0,0 +1,83 @@
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+
+const randomIntBetween = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const capitalize = string => string.charAt(0).toUpperCase() + string.slice(1);
+
+const get = url => {
+  return new Promise((resolve, reject) => {
+    https.get(url, res => {
+      let result = '';
+      res.on('data', data => {
+        result += data.toString();
+      });
+      res.on('end', data => {
+        resolve(result);
+      });
+    }).on('error', error => {
+      reject(error);
+    });
+  });
+};
+
+const getHipsum = async (params = { paras: 1 }) => {
+  const paramsArray = [];
+
+  for (let param in params) {
+    paramsArray.push(`${param}=${params[param]}`);
+  }
+
+  const url = `https://hipsum.co/api/?type=hipster-centric&${paramsArray.join('&')}`;
+  const data = await get(url);
+  return JSON.parse(data);
+};
+
+const generateData = async () => {
+  const images = [];
+  const workspaces = [];
+  let lastPhotoId = 1;
+
+  const data = await getHipsum({ paras: 100 });
+
+  for (let i = 0; i < data.length; i++) {
+    const paragraph = data[i].replace(/\s+/g, ' ');
+    const sentences = paragraph.split('. ');
+
+    const name = sentences.pop().slice(0, -1);
+    const url = name.toLowerCase().replace(/[,.?'"]/g, '').split(' ').join('-');
+    const headline = sentences.pop();
+    const trimmedParagraph = sentences.join('. ') + '.';
+
+    const workspace = {
+      id: i,
+      name: name,
+      url: url,
+      descriptionHeadline: headline,
+      description: trimmedParagraph,
+    };
+
+    workspaces.push(workspace);
+
+    const limit = lastPhotoId + randomIntBetween(1, 7);
+    const words = workspace.name.toLowerCase().split(' ');
+
+    for (; lastPhotoId < limit; lastPhotoId++) {
+      const description = words[randomIntBetween(0, words.length - 1)];
+
+      const image = {
+        id: lastPhotoId,
+        workspaceId: i,
+        url: `/api/photos/${lastPhotoId}`,
+        description: capitalize(description),
+      };
+
+      images.push(image);
+    }
+  }
+
+  console.log(images);
+};
+
+generateData();
+
